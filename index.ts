76139f1999a7977271953d26788dbe80dd03df02
COCKTAIL <aristovstas2004@gmail.com>
2023-08-23 12:23:41 +0400

    add merge2SortedLists/v1 aka whileLoop

--- index.ts
+++ index.ts
@@ -2,11 +2,13 @@ import mergeTwoLists, {ListNode} from "./mergeTwoSortedLists/v1";
 
 (() => {
     console.log(
-        mergeTwoLists(
-            new ListNode(
-                1, new ListNode(2, new ListNode(4)),
-            ),
-            new ListNode(1, new ListNode(3, new ListNode(4)),
+        JSON.stringify(
+            mergeTwoLists(
+                new ListNode(
+                    1, new ListNode(2, new ListNode(4)),
+                ),
+                new ListNode(1, new ListNode(3, new ListNode(4)),
+                ),
             ),
         ),
     );
--- mergeTwoSortedLists/v1/index.ts
+++ mergeTwoSortedLists/v1/index.ts
@@ -10,7 +10,35 @@ export class ListNode {
 }
 
 function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
-    return null;
+    let list1Node: ListNode = list1;
+    let list2Node: ListNode = list2;
+    if (!list1Node) {
+        return list2Node;
+    } else if (!list2Node) {
+        return list1Node;
+    }
+    const resListNode = new ListNode(-1); // val = 0
+    let resListNodeNode = resListNode;
+    const getMin = (a: number, b: number) => a > b ? b : a;
+    while (list1Node || list2Node) {
+        if (!list1Node) {
+            resListNodeNode.next = list2Node;
+            break;
+        } else if (!list2Node) {
+            resListNodeNode.next = list1Node;
+            break;
+        }
+        const min = getMin(list1Node.val, list2Node.val);
+        if (min === list1Node.val) {
+            resListNodeNode.next = list1Node;
+            list1Node = list1Node.next;
+        } else {
+            resListNodeNode.next = list2Node;
+            list2Node = list2Node.next;
+        }
+        resListNodeNode = resListNodeNode.next;
+    }
+    return resListNode.next;
 }
 
 export default mergeTwoLists;
